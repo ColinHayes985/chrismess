9de0d059e740390b64dd9a4c3e3c17bcb7d781fd
Colin Hayes <colin.hayes985@gmail.com>
2018-06-25 14:03:40 -0400

    Added submit button and empty form. Submit button functions the same as 'Change Heading'.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('button');
 const bonusButton = document.querySelector('#bonusCreditButton');
+const submitButton = document.querySelector('#submit');
 
 function changeHeading(){
     const h1 = document.querySelector('h1');
@@ -11,5 +12,11 @@ function changeHeadingBonus(){
     h1.textContent='Chris Pratt controversial actions during Infinity War were very in character for Star-Lord';
 }
 
+function submit(){
+    const h1 = document.querySelector('h1');
+    h1.textContent='Chris';
+}
+
 button.addEventListener('click', changeHeading);
 bonusButton.addEventListener('click', changeHeadingBonus);
+submitButton.addEventListener('click', submit);
