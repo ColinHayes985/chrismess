056c4b4e1ffc15d5ce6b642cb733b34a28e2b629
Colin Hayes <colin.hayes985@gmail.com>
2018-06-25 13:48:54 -0400

    Standard Homework now functioning

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const button = document.querySelector('button')
+const button = document.querySelector('button');
+const bonusButton = document.querySelector('#bonusCreditButton');
 
 function changeHeading(){
-    const h1 = document.querySelector('h1')
-    h1.textContent='Chris'
+    const h1 = document.querySelector('h1');
+    h1.textContent='Chris';
 }
 
-button.onclick(changeHeading);
+function changeHeadingBonus(){
+    const h1 = document.querySelector('#bonusCreditHeading');
+    h1.textContent='Chris Pratt controversial actios durig Infinity War were very in character for Star-Lord';
+}
+
+button.addEventListener('click', changeHeading);
+bonusButton.addEventListener('click', changeHeadingBonus);
